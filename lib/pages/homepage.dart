5ebb9f762fb646afed703247e00e625c122faf20
Ibrahim Itani <ibrahm.itani@gmail.com>
2025-11-26 18:59:03 +0200

    added ignore command to package

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:item_count_number_button/item_count_number_button.dart';
 
 class HomePage extends StatefulWidget {
